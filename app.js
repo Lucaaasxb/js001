2a8ad19d242459e8716c7f95adb9a9e2093556ce
Lucaaasxb <lucasbriansilvafagundes@gmail.com>
2025-05-08 20:16:26 -0300

    tipos de dados

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 // Declaração de variáveis(var, let, const)
 let nome = "Lucas"
 let idade = 16
-
+let espaco = " "
 
 
 // Console e Debug
@@ -30,10 +30,32 @@ console.log(typeof salario)
 console.log(typeof fumante)
 
 // Operadores
-//     Operador Atribuição
-//     Operadores aritméticos
-//     Operadores relacionais
-//     Operadores lógicos
+//     Operador Atribuição(=)
+//     Operadores aritméticos(+, -, /, *, %)
+console.log(10 + 15)
+let n1 = 10
+let n2 = 5
+console.log(n1 + n2)  //soma
+console.log(n1 - n2)  //subtração
+console.log(n1 * n2)  //multiplicação
+console.log(n1 / n2)  //divisão
+console.log(n1 % n2)  //resto da divisão
+console.log(espaco)
+//     Operadores relacionais(>, <, >=, <=, ==, !=)
+console.log(n1==n2) //igualdade
+console.log(n1!=n2) //diferente
+console.log(n1>n2) //maior
+console.log(n1<n2) //menor
+console.log(n1>=n2) //maior ou igual
+console.log(n1<=n2) //menor ou igual
+console.log(espaco) 
+//     Operadores lógicos(!, &&, ||)
+console.log(!10>2) // ! - inverte o resultado, ser for verdadeiro vira falso, se for falso vira verdadeiro
+console.log(!false)
+console.log(10>2 && 35<100 && n1>n2) // && - todas as verificações precisam ser verdadeiras para o resultado ser verdadeiro
+console.log(10>2 || 100==150 || 57==57) // || - apenas uma verificação precisa ser verdadeira para o resultado ser verdadeiro
+console.log(espaco)
+
 
 // Estrutura
 //     Estrutura de controle/decisão
